9eda90013e6b93fc40161ef0567ec5e4ab214365
profejuanjose17-afk <profejuanjose17@gmail.com>
2026-07-12 11:25:24 -0600

    chore: configurar Prisma y validar conexión con PostgreSQL

--- index.js
+++ index.js
@@ -9,31 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "API Restaurante funcionando"
-    });
-});
-
 const PORT = process.env.PORT || 3000;
-
-async function conectarBD() {
-    try {
-        await prisma.$connect();
-        console.log("Base de datos conectada.");
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-conectarBD();
-
-async function prueba() {
-    const mesas = await prisma.mesas.findMany();
-    console.log(mesas);
-}
-
-prueba();
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);

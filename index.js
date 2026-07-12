fa14c840e6cf6a600ff4cc118a4944d90f9d7e48
profejuanjose17-afk <profejuanjose17@gmail.com>
2026-07-12 09:47:31 -0600

    chore: inicializar proyecto Express con Prisma

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "API Restaurante funcionando"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
